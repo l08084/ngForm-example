197a82edaa44cab1b3f73f679b7e6e259382ccd9
l08084 <l08084@shibaura-it.ac.jp>
2016-12-31 15:28:38 +0900

    add ng2-bootstrap, input form, submit button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  public onSubmit(f: NgForm) {
+    console.log(f.value);
+  }
 }
